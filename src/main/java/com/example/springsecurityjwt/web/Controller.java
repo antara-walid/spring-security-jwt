ff48aaff752ea4ebf0165c8e03a08647f330ff76
antara-walid <Antara.walid.27@gmail.com>
2022-08-25 09:13:43 +0100

    first commit

---
+++ src/main/java/com/example/springsecurityjwt/web/Controller.java
@@ -0,0 +1,13 @@
+package com.example.springsecurityjwt.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+    @RequestMapping(value = "/hello")
+    public String hello()
+    {
+        return "hello";
+    }
+}
