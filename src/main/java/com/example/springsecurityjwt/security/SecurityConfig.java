88991986d11557899c76ec68ef1743260bd62fdf
antara-walid <Antara.walid.27@gmail.com>
2022-08-25 09:38:54 +0100

    add the JwtUtil class

--- src/main/java/com/example/springsecurityjwt/security/SecurityConfig.java
+++ src/main/java/com/example/springsecurityjwt/security/SecurityConfig.java
@@ -1,10 +1,13 @@
 package com.example.springsecurityjwt.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -14,5 +17,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder()
+    {
+        return NoOpPasswordEncoder.getInstance();
     }
 }
--- src/main/java/com/example/springsecurityjwt/service/MyUserDetailService.java
+++ src/main/java/com/example/springsecurityjwt/service/MyUserDetailService.java
@@ -1,15 +1,18 @@
 package com.example.springsecurityjwt.service;
 
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
 
 
 @Service
 public class MyUserDetailService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return new User("user" ,"user");
+        return new User("user" ,"user",new ArrayList<>()); // for simplicity the user is hard coded
     }
 }
